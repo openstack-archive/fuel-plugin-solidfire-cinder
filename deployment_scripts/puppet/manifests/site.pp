04a325758f28f8b2e06017b1e36a6fa9d47bc2ff
Ed Balduf <ed.balduf@solidfire.com>
2016-05-09 09:21:30 -0600

    More documentation changes per Mirantis request.
    Change-Id: I2276be3f2c5f1bf7a6769ca0bf200ba521b84f47
    
    Refactoring for the new structure for Fuel 8. This change now handles
    multi-backend in the face of the default options (lvm, ceph) and the
    NetApp plugin. Note: I removed the multi-backend option, so it will
    always put SolidFire in it's own stanza along with everyone (in the above
    list) also.
    
    Change-Id: Ibc3ffe202fb405da9b566498c23f6bcda84080a9

---
+++ deployment_scripts/puppet/manifests/site.pp
@@ -0,0 +1,6 @@
+notice('MODULAR: fuel-plugin-solidfire-cinder')
+
+# The manifest is executed on nodes with cinder role or on nodes with controller role when Ceph is used.
+if ('cinder' in hiera('roles')) or (!empty(filter_nodes(hiera('nodes'), 'role', 'ceph-osd'))) {
+  class { 'plugin_cinder_solidfire': }
+}
